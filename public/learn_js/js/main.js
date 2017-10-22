8458b717999bf212d5d3b171a7faad5b8af5a89f
Junyoung Kim <migkjy@naver.com>
2017-10-22 15:44:02 +0900

    write main.js

---
+++ public/learn_js/js/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SyleSheet, text, View } from 'react-native';
+
+export default class App extends React.Componet {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Hello World</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
