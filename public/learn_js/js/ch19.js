b4e2dc17cdeef11caf8e6bc0c85b6349f794ff1e
Junyoung Kim <migkjy@naver.com>
2017-10-29 14:35:17 +0900

    ch19 finished

---
+++ public/learn_js/js/ch19.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  const $paras = $('p');
+  console.log($paras);
+  console.log(typeof $paras);
+  console.log($paras instanceof $);
+  console.log($paras instanceof jQuery);
+
+  // 나머지 제이쿼리는 작업하면서 공부
+});
